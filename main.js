d9fd7d8a4a5dc544a8efce938ffef0b7abcf187d
teresa2905 <s345368@oslomet.no>
2021-11-17 22:33:42 +0100

    uten javascript:)

--- main.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "40%";
-    document.getElementById("main").style.marginLeft = "50%";
-  }
-  
-  function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
